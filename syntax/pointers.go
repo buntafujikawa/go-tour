fff07fae538c9c4db388f0fdcf5c7b58f40a73c1
buntafujikawa <bunta.fujikawa@gmail.com>
2019-04-05 21:46:58 +0900

    add pointers

---
+++ syntax/pointers.go
@@ -0,0 +1,18 @@
+package syntax
+
+import "fmt"
+
+func main() {
+    i, j := 42, 2701
+    
+    // & オペレータは、そのオペランド( operand )へのポインタを引き出します。
+    p := &i
+    // * オペレータは、ポインタの指す先の変数を示します。
+    fmt.Println(*p)
+    *p = 21
+    fmt.Println(i)
+    
+    p = &j
+    *p = *p / 37
+    fmt.Println(j)
+}
